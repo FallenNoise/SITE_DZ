e4a10276a40e59a4c38991363e09e5b5eedaab12
Бойбородов Илья <fflash08@mail.ru>
2026-08-31 11:14:13 +0300

    feat(shop): добавить модель Product и применить миграции базы данных

--- shop/models.py
+++ shop/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+class Product(models.Model):
+    name = models.CharField(max_length=200, verbose_name='Название товара')
+    description = models.TextField(verbose_name='Описание', blank=True)
+    price = models.DecimalField(max_digits=10, decimal_places=2, verbose_name='Цена')
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name='Дата добавления')
+
+    class Meta:
+        verbose_name = 'Товар'
+        verbose_name_plural = 'Товары'
+
+    def __str__(self):
+        return self.name
