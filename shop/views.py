d14db34549b18ef7e1bcbe2ff8d1052a9d2fa41b
Бойбородов Илья <fflash08@mail.ru>
2026-09-07 15:38:01 +0300

    feat(shop): реализовать view add_product для обработки добавления товара

--- shop/views.py
+++ shop/views.py
@@ -1,7 +1,21 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Product
+from .forms import ProductForm
 
 
 def product_list(request):
     products = Product.objects.all().order_by('-created_at')
     return render(request, 'shop/index.html', {'products': products})
+
+
+def add_product(request):
+    if request.method == 'POST':
+        form = ProductForm(request.POST)
+        if form.is_valid():
+            form.save()
+            # Убедитесь, что 'index' - это имя маршрута главной страницы каталога
+            return redirect('index')
+    else:
+        form = ProductForm()
+
+    return render(request, 'shop/add_product.html', {'form': form})
