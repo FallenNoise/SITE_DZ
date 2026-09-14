c7a6fd734da4d941cb0aa5dafc30e4fbe3f8c2b3
Бойбородов Илья <fflash08@mail.ru>
2026-09-14 13:17:47 +0300

    feat(shop): добавить маршруты /edit/ и /delete/ для товаров

--- shop/urls.py
+++ shop/urls.py
@@ -4,4 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.product_list, name='index'),
     path('add/', views.add_product, name='add_product'),
+    path('edit/<int:product_id>/', views.edit_product, name='edit_product'),
+    path('delete/<int:product_id>/', views.delete_product, name='delete_product'),
 ]
